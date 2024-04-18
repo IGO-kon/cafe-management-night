467ee19e5e46b7813d4636cf2c2c1824f32ab365
Gakufumi Tahara <alexsan0115@gmail.com>
2024-04-18 20:40:16 +0900

    商品登録ページから送信した内容がPythonで受け取れるようにしました

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/product/add', methods=['GET'])
+@app.route('/product/add', methods=['GET', 'POST'])
 def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        category = request.form['category']
+        price = float(request.form['price'])
+
+        print(f"商品名: {name}")
+        print(f"カテゴリ: {category}")
+        print(f"価格: {price}")
+
+        return redirect(url_for('add_product'))
+
     return render_template('add_product.html')
 
 if __name__ == '__main__':
